312e6b30c0b12bece9a745cc6452cc62d9d23aa5
Selina-Knaepen <knaepen.selina@gmail.com>
2020-10-25 21:02:27 +0100

    Initialize frontend project

---
+++ frontend/home_light/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(HomeLight());
+
+class HomeLight extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          backgroundColor: Colors.teal,
+          bottomNavigationBar: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home),),
+              Tab(icon: Icon(Icons.lightbulb_outline),),
+              Tab(icon: Icon(Icons.alarm),),
+            ]
+          ),
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              MyHomePage(title: 'Home'),
+              MyHomePage(title: 'Lights'),
+              MyHomePage(title: 'Automation')
+            ]
+          ),
+        ),
+      )
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Text('Hello Android'),
+    );
+  }
+}
