aaed5fc32a98a32eb1bc5755629f8dc62b69c890
Selina-Knaepen <knaepen.selina@gmail.com>
2020-10-26 11:00:57 +0100

    Add light button

--- frontend/home_light/lib/home.dart
+++ frontend/home_light/lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Home extends StatefulWidget
 {
@@ -21,14 +22,29 @@ class HomeState extends State<Home>
       ),
       body: Column(
         children: <Widget>[
-          Text('This is the first line'),
-          Text('This is the second line'),
+          Text('[Name light]'),
+          MaterialButton(
+            shape: CircleBorder(),
+            child: Text('Light'),
+            color: Colors.amber,
+            onPressed: () { _turnOnLight(); },
+          ),
           RaisedButton(
-            onPressed: () {},
+            onPressed: () { _savePreset(); },
             child: Text('Save Preset'),
           )
         ],
       ),
     );
+  }
+
+  void _turnOnLight()
+  {
+    Fluttertoast.showToast(msg: 'Lights have been turned on');
+  }
+
+  void _savePreset()
+  {
+
   }
 }
